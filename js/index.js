3f7466209389d3c267052db1084bf84e9263beb7
Hritik262 <hritikraj121@gmail.com>
2023-11-04 21:47:42 +0530

    Added scroll in animation in cards

--- js/index.js
+++ js/index.js
@@ -1,41 +1,11 @@
-function isElementInViewport(el) {
-    const rect = el.getBoundingClientRect();
-    return (
-      rect.top >= 0 &&
-      rect.left >= 0 &&
-      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-    );
-  }
-  
-  function animateCardsOnScroll() {
-    const sdCards = document.querySelectorAll('.sd-card');
-    let animationDone = false;
-  
-    function checkVisibility() {
-      if (!animationDone) {
-        sdCards.forEach((sdCard, index) => {
-          if (isElementInViewport(sdCard)) {
-            sdCard.classList.add('animate__animated', 'animate__fadeInUp');
-            sdCard.style.opacity = 1;
-            sdCard.style.transform = 'translateY(0)';
-            sdCard.style.transitionDelay = `${index * 0.2}s`; // Add a delay between each card
-          }
-        });
-  
-        animationDone = Array.from(sdCards).every(
-          (sdCard) =>
-            sdCard.classList.contains('animate__animated', 'animate__fadeInUp') &&
-            sdCard.style.opacity === '1' &&
-            sdCard.style.transform === 'translateY(0)'
-        );
-      }
+const observer = new IntersectionObserver((entries) =>{
+  entries.forEach((entry) => {
+    console.log(entry)
+    if(entry.isIntersecting){
+      entry.target.classList.add('show');
     }
-  
-    checkVisibility();
-  
-    window.addEventListener('scroll', checkVisibility);
-  }
-  
-  animateCardsOnScroll();
-  
+  });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
